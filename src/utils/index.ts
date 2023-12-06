e7b7fe565d9c35f46efca90466f4a71e71c0c953
dodoDawn <749095258@qq.com>
2023-12-07 02:38:28 +0800

    feat: test

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,1 @@
-export const isDev = () => {
-  console.log('[dodo] ', 'process.env.NODE_ENV', process.env.NODE_ENV);
-  return process.env.NODE_ENV === 'development';
-};
+export const isDev = process.env.NODE_ENV === 'development';
