9f3b703157ccaaa9f4275435836e0152d613938a
tangqiuqiu <tangqiuqiu@pinduoduo.com>
2024-01-03 14:01:24 +0800

    feat: 组件库

--- src/pages/amazing/App.tsx
+++ src/pages/amazing/App.tsx
@@ -1,11 +1,16 @@
 import * as React from 'react';
 import * as styles from './App.module.less';
+import { toast } from 'sweet-me';
 
-interface IProps {}
+interface IProps { }
 
 export const App = (props: IProps) => {
   React.useEffect(() => {
     console.log('[dodo] ', 'home');
+
+    setTimeout(() => {
+      toast('123123213123213');
+    }, 3000);
   }, []);
 
   return <div className={styles.app}>Home</div>;
