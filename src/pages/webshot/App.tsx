8a566458fff577c5057b940bd7636de9e097ff5e
tangqiuqiu <dawn.tang@temu.com>
2024-01-25 19:30:46 +0800

    feat: request bug fix

--- src/pages/webshot/App.tsx
+++ src/pages/webshot/App.tsx
@@ -7,16 +7,27 @@ import { useFetch } from '@/utils/fetch';
 interface ApiResponse {
   cover: string;
   title: string;
+  date: string;
+  url: string;
   favicon: string;
 }
 
 export const App = () => {
   const [url, setUrl] = useState("");
-  const { data: response, runApi, loading: isLoading, error } = useFetch<ApiResponse>({
+  const { data: response, runApi } = useFetch<ApiResponse>({
     url: "/web-info",
     params: { url },
     loadingFn: () => loading('加载中')
   });
+
+  const { data: listData, runApi: runListApi } = useFetch<ApiResponse[]>({
+    url: "/web-info/list",
+    params: { url },
+    autoRun: true,
+    loadingFn: () => loading('列表加载中...', undefined, false)
+  });
+
+  console.log('[dodo] ', 'listData', listData);
 
   const handleButtonClick = async () => {
     if (!validUrl(url)) {
@@ -30,7 +41,7 @@ export const App = () => {
 
   return (
     <Page className={styles.page}>
-      <Header title="网页信息抓取" />
+      <Header title="网页信息抓取" isSticky />
       <div className={styles.inputContainer}>
         <Input
           onValueChange={(val = '') => setUrl(val.trim())}
@@ -38,7 +49,7 @@ export const App = () => {
         />
         <Button onClick={handleButtonClick}>开始抓取</Button>
       </div>
-      {response && (
+      {response ? (
         <div className={styles.responseContainer}>
           <Title>网页标题</Title>
           <div className={styles.titleWrap}>
@@ -48,7 +59,24 @@ export const App = () => {
           <Title>网页截图</Title>
           <img className={styles.cover} src={response.cover} alt="网页截图" />
         </div>
+      ) : (
+        <div className={styles.emptyHolder}>
+          待抓取
+        </div>
       )}
+
+      <Title className={styles.listTitle}>已抓取页面</Title>
+      <div className={styles.listWrap}>
+        {listData?.map(info => (
+          <div className={styles.responseContainer} key={info.url}>
+            <Title className={styles.itemTitle}>
+              <img className={styles.icon} src={info.favicon} alt="网页图标" />
+              <div className={styles.title}>{info.title}</div>
+            </Title>
+            <img className={styles.cover} src={info.cover} alt="网页截图" />
+          </div>
+        ))}
+      </div>
     </Page>
   );
 };
