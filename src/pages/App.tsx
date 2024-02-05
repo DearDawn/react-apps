fad7f356da75e42a0472ac15176f0dbdd73186be
tangqiuqiu <dawn.tang@temu.com>
2024-02-05 09:53:13 +0800

    feat: index page

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -13,9 +13,10 @@ export const App = (props: IProps) => {
     <Page maxWidth='100vw' className={styles.app}>
       <Header title="小糖的 React 项目合集" isSticky />
       <Title className={styles.title}><a href="./home/">Home 页</a></Title>
-      <Title className={styles.title}><a href="./home/">短链接生成器</a></Title>
-      <Title className={styles.title}><a href="./home/">网页截图</a></Title>
-      <Title className={styles.title}><a href="./home/">破碎 & 完整</a></Title>
+      <Title className={styles.title}><a href="./nine/">几点下班</a></Title>
+      <Title className={styles.title}><a href="./link/">短链接生成器</a></Title>
+      <Title className={styles.title}><a href="./webshot/">网页截图</a></Title>
+      <Title className={styles.title}><a href="./piece/">破碎 & 完整</a></Title>
     </Page>
   )
 }
