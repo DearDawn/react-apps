c0f10b973edad6fb34f33dad2312140d07c59888
tangqiuqiu <dawn.tang@temu.com>
2024-04-08 15:35:45 +0800

    feat: host

--- src/pages/share/socket.ts
+++ src/pages/share/socket.ts
@@ -2,7 +2,7 @@ import { isDev } from '@/utils';
 import { io } from 'socket.io-client';
 
 
-const socket = io(isDev ? 'http://localhost:7020' : `${location.origin}:9010`, {
+const socket = io(isDev ? 'http://localhost:7020' : `${location.origin}:7020`, {
   transports: ['websocket']
 }); // 连接到服务器的Socket.IO实例
 
