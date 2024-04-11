90c89058e4568c49ee57319e4bdd49852fbb23e4
dodoDawn <749095258@qq.com>
2024-04-11 23:28:56 +0800

    feat: bugfix

--- src/pages/share/socket.ts
+++ src/pages/share/socket.ts
@@ -1,8 +1,7 @@
 import { isDev } from '@/utils';
 import { io } from 'socket.io-client';
 
-
-const socket = io(isDev ? 'http://localhost:7020' : `${location.origin}:7020`, {
+const socket = io(isDev ? `http://${location.hostname}:7020` : `${location.origin}:7020`, {
   transports: ['websocket']
 }); // 连接到服务器的Socket.IO实例
 
