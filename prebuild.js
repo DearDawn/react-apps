5c1a3008ea75f87f5076e65df47a939522a2c05c
tangqiuqiu <dawn.tang@temu.com>
2024-04-16 18:09:48 +0800

    构建信息

--- prebuild.js
+++ prebuild.js
@@ -1,14 +1,22 @@
 const fs = require('fs');
-const packageJson = require('./package.json');
 
 // 获取当前日期和时间
 const buildTime = new Date().toISOString();
 
-// 获取当前版本号
-const version = packageJson.version;
+// 读取 .env 文件内容
+const envContent = fs.readFileSync('.env', 'utf8');
+
+// 解析 .env 文件内容，获取上一次的版本号
+const previousVersion = envContent.match(/VERSION=(\d*)/)[1];
+
+// 将上一次的版本号转换为数字并自增
+const incrementedVersion = parseInt(previousVersion) + 1;
+
+// 构建新的版本号
+const newVersion = incrementedVersion.toString();
 
 // 构建要写入的内容
-const envContent = `BUILD_TIME=${buildTime}\nVERSION=${version}`;
+const newEnvContent = `BUILD_TIME=${buildTime}\nVERSION=${newVersion}`;
 
-// 将内容写入到 .env 文件
-fs.writeFileSync('.env', envContent);
+// 将新的内容写入到 .env 文件
+fs.writeFileSync('.env', newEnvContent);
