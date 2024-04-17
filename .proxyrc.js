4b9f723e2bac56eb837af5970cca2f6cb96d47f8
tangqiuqiu <dawn.tang@temu.com>
2024-04-17 12:02:31 +0800

    feat: 模型交互

--- .proxyrc.js
+++ .proxyrc.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 const useLocal = process.argv.at(-1) !== '--open';
 
-module.exports =  function (app) {
+module.exports = function (app) {
   app.use(
     createProxyMiddleware("/api", {
       target: useLocal ? "http://localhost:7020/" : "https://www.dododawn.com:7020/",
--- src/pages/pet/App.tsx
+++ src/pages/pet/App.tsx
@@ -3,20 +3,24 @@ import * as styles from './App.module.less';
 import * as PIXI from 'pixi6.js';
 import { Live2DModel } from 'pixi-live2d-display';
 
+// expose PIXI to window so that this plugin is able to
+// reference window.PIXI.Ticker to automatically update Live2D models
+// @ts-ignore
+// PIXI.Renderer.registerPlugin('interaction', PIXI.InteractionManager);
+window.PIXI = PIXI;
+
 export const App = () => {
   useEffect(() => {
-    // expose PIXI to window so that this plugin is able to
-    // reference window.PIXI.Ticker to automatically update Live2D models
-    // @ts-ignore
-    window.PIXI = PIXI;
-
     const app = new PIXI.Application({
+      autoStart: true,
+      resizeTo: window,
       view: document.getElementById('dodo-game-root') as HTMLCanvasElement,
     });
 
     // no `await` here as it's not a Promise
     const model = Live2DModel.fromSync(
-      'https://dododawn-1300422826.cos.ap-shanghai.myqcloud.com/public/models/wanko/wanko.model.json'
+      'https://dododawn-1300422826.cos.ap-shanghai.myqcloud.com/public/models/wanko/wanko.model.json',
+      { autoInteract: true }
     );
 
     model.once('load', () => {
@@ -25,14 +29,34 @@ export const App = () => {
 
       model.anchor.set(0.5, 0.5);
       model.scale.set(0.3, 0.3);
-      model.position.set(
-        (window.innerWidth - model.width * 0.3) / 2,
-        (window.innerHeight - model.height * 0.3) / 2
-      );
+      model.position.set(window.innerWidth / 2, window.innerHeight / 2);
 
-      model.on('hit', () => {
-        model.motion('Tap@Body');
+      model.on('pointerdown', () => {
+        console.log('[dodo] ', 'hit');
+        // model.motion('idle');
+        model.motion('shake');
+        // model.motion('tap_body');
       });
+
+      function draggable(model) {
+        model.buttonMode = true;
+        model.on('pointerdown', (e) => {
+          model.dragging = true;
+          model._pointerX = e.data.global.x - model.x;
+          model._pointerY = e.data.global.y - model.y;
+        });
+        model.on('pointermove', (e) => {
+          if (model.dragging) {
+            model.position.x = e.data.global.x - model._pointerX;
+            model.position.y = e.data.global.y - model._pointerY;
+          }
+        });
+        model.on('pointerupoutside', () => (model.dragging = false));
+        model.on('pointerup', () => (model.dragging = false));
+      }
+
+      // 绑定模型拖拽方法
+      draggable(model);
     });
   }, []);
 
