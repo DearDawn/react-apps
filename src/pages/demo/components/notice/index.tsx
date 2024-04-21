f17c4a7a341237dcf1c6b146ff6bb58e8770299b
tangqiuqiu <dawn.tang@temu.com>
2024-04-21 11:26:05 +0800

    feat: app 图标

--- src/pages/demo/components/notice/index.tsx
+++ src/pages/demo/components/notice/index.tsx
@@ -2,8 +2,22 @@ import { Button } from 'sweet-me';
 import * as styles from './index.module.less';
 import Image from 'src/assets/bunny.png';
 import { Comp } from '../type';
+import { useCallback, useState } from 'react';
 
 export const Notice: Comp = () => {
+  const [badge, setBadge] = useState(0);
+
+  const handleBadge = useCallback(() => {
+    const newVal = (badge + 1) % 10;
+
+    if (newVal === 0) {
+      navigator?.clearAppBadge();
+    } else {
+      navigator?.setAppBadge(newVal);
+    }
+    setBadge(newVal);
+  }, [badge]);
+
   const handleClick = () => {
     if ('Notification' in window) {
       alert(Notification?.permission);
@@ -11,17 +25,19 @@ export const Notice: Comp = () => {
       alert('不支持');
     }
 
-    Notification.requestPermission().then((result) => {
-      alert(`result${result}`);
-    }).catch(err => {
-      alert(`err${JSON.stringify(err)}`);
-    });
+    Notification.requestPermission()
+      .then((result) => {
+        alert(`result${result}`);
+      })
+      .catch((err) => {
+        alert(`err${JSON.stringify(err)}`);
+      });
   };
 
   const handleNotice = () => {
-    const notice = new Notification("你好呀~ 我是一个通知", {
+    const notice = new Notification('你好呀~ 我是一个通知', {
       body: 'Notification Body',
-      icon: Image
+      icon: Image,
     });
 
     notice.onshow = () => {
@@ -33,29 +49,31 @@ export const Notice: Comp = () => {
     };
   };
 
-  function showNotification () {
+  function showNotification() {
     Notification.requestPermission().then((result) => {
       console.log('[dodo] ', 'result', result, navigator.serviceWorker);
-      if (result === "granted") {
-        navigator.serviceWorker.ready.then((registration) => {
-          console.log('[dodo] ', '1111', 1111);
-          registration.showNotification("Vibration Sample", {
-            body: "Buzz! Buzz!",
-            // icon: "../images/touch/chrome-touch-icon-192x192.png",
-            // vibrate: [200, 100, 200, 100, 200, 100, 200],
-            // tag: "123",
-          });
-          setTimeout(() => {
-            registration.showNotification("Vibration Sample123", {
-              body: "Buzz! Buzz!",
+      if (result === 'granted') {
+        navigator.serviceWorker.ready
+          .then((registration) => {
+            console.log('[dodo] ', '1111', 1111);
+            registration.showNotification('Vibration Sample', {
+              body: 'Buzz! Buzz!',
               // icon: "../images/touch/chrome-touch-icon-192x192.png",
               // vibrate: [200, 100, 200, 100, 200, 100, 200],
               // tag: "123",
             });
-          }, 5000);
-        }).catch(err => {
-          console.log('[dodo] ', 'err', err);
-        });
+            setTimeout(() => {
+              registration.showNotification('Vibration Sample123', {
+                body: 'Buzz! Buzz!',
+                // icon: "../images/touch/chrome-touch-icon-192x192.png",
+                // vibrate: [200, 100, 200, 100, 200, 100, 200],
+                // tag: "123",
+              });
+            }, 5000);
+          })
+          .catch((err) => {
+            console.log('[dodo] ', 'err', err);
+          });
       }
     });
   }
@@ -72,6 +90,7 @@ export const Notice: Comp = () => {
       <Button onClick={handleClick}>授权</Button>
       <Button onClick={handleNotice}>通知</Button>
       <Button onClick={handleNoticeSW}>SW 通知</Button>
+      <Button onClick={handleBadge}>Badge {badge}</Button>
     </div>
   );
 };
