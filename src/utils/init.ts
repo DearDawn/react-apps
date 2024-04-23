4b6122bfe0a7702aa231c60c8aeaa074f4067ac4
tangqiuqiu <dawn.tang@temu.com>
2024-04-23 13:58:13 +0800

    feat: vconsole 注入

--- src/utils/init.ts
+++ src/utils/init.ts
@@ -1,2 +1,19 @@
-console.log(`%c[react-apps] 构建时间%c${process.env.BUILD_TIME}`, 'background: #b2f2bb;padding: 4px 8px;', 'background: #bbb2f2; padding: 4px 8px;');
-console.log(`%c[react-apps] 构建版本%c${process.env.APP_VERSION}`, 'background: #b2f2bb;padding: 4px 8px;', 'background: #bbb2f2; padding: 4px 8px;');
+import VConsole from 'vconsole';
+
+console.log(
+  `%c[react-apps] 构建时间%c${process.env.BUILD_TIME}`,
+  'background: #b2f2bb;padding: 4px 8px;',
+  'background: #bbb2f2; padding: 4px 8px;'
+);
+console.log(
+  `%c[react-apps] 构建版本%c${process.env.APP_VERSION}`,
+  'background: #b2f2bb;padding: 4px 8px;',
+  'background: #bbb2f2; padding: 4px 8px;'
+);
+
+const urlParams = new URLSearchParams(window.location.search);
+const testParam = urlParams.get('test');
+
+if (testParam === '1') {
+  new VConsole();
+}
