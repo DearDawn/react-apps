81bf2ba2522ef82c7e1f46cb939618114444d8c6
tangqiuqiu <dawn.tang@temu.com>
2024-04-24 21:31:32 +0800

    feat: 压缩图片

---
+++ src/utils/image.ts
@@ -0,0 +1,62 @@
+export const compressImage = ({
+  file,
+  outputFileName,
+  quality,
+  scaleSize = 50,
+  callback,
+}) => {
+  const fileURL = URL.createObjectURL(file);
+
+  // 创建一个图片元素
+  const img = new Image();
+
+  // 当图片加载完成时
+  img.onload = function () {
+    // 创建一个canvas元素
+    const canvas = document.createElement('canvas');
+    const ctx = canvas.getContext('2d');
+    // 计算缩放后的宽高
+
+    let scaledWidth = Math.min(scaleSize, img.width);
+    let scaledHeight = Math.min(
+      (scaleSize / img.width) * img.height,
+      img.height
+    );
+
+    if (img.width < img.height) {
+      scaledHeight = Math.min(scaleSize, img.height);
+      scaledWidth = Math.min((scaleSize / img.height) * img.width, img.width);
+    }
+
+    // 设置canvas的宽高与图片一致
+    canvas.width = scaledWidth;
+    canvas.height = scaledHeight;
+
+    // 在canvas上绘制原始图片
+    ctx.drawImage(img, 0, 0, scaledWidth, scaledHeight);
+
+    // 使用canvas的toDataURL方法来压缩图像
+    // 第二个参数为压缩质量，范围为0到1之间
+    const compressedDataUrl = canvas.toDataURL('image/png', quality);
+
+    // 将Data URL转换为Blob对象
+    const byteString = atob(compressedDataUrl.split(',')[1]);
+    const ab = new ArrayBuffer(byteString.length);
+    const ia = new Uint8Array(ab);
+    for (let i = 0; i < byteString.length; i++) {
+      ia[i] = byteString.charCodeAt(i);
+    }
+    const blob = new Blob([ab], { type: 'image/png' });
+
+    // 构造一个新的File对象
+    const compressedFile = new File([blob], outputFileName, {
+      type: 'image/png',
+    });
+
+    // 调用回调函数，传递压缩后的文件对象
+    callback(compressedFile);
+  };
+
+  // 将文件数据赋值给图片元素的src属性
+  img.src = fileURL;
+};
