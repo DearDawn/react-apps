fc253e6103e326982a4566d8d6986adaf5492df1
tangqiuqiu <dawn.tang@temu.com>
2024-04-24 10:24:37 +0800

    feat: button

--- src/pages/pet/game/menu.ts
+++ src/pages/pet/game/menu.ts
@@ -1,9 +1,11 @@
 import * as PIXI from 'pixi6.js';
+import { Button } from './button';
 
 export class Menu extends PIXI.Container {
   app: PIXI.Application = null;
   background: PIXI.Graphics;
-  button: PIXI.Text;
+  button: Button;
+  buttonText: PIXI.Text;
   constructor({ app, text = '开始游戏' }) {
     super();
 
@@ -14,27 +16,25 @@ export class Menu extends PIXI.Container {
     this.background.drawRect(0, 0, app.screen.width, app.screen.height);
     this.background.endFill();
 
-    // 创建开始游戏按钮
-    this.button = new PIXI.Text(text, {
-      fontFamily: 'Arial',
-      fontSize: 36,
-      fill: 0xffffff,
+    // 创建按钮背景
+    this.button = new Button({
+      width: 300,
+      height: 100,
+      app: this.app,
+      text,
+      textStyle: { fontFamily: 'Arial', fontSize: 36, fill: 0x000000 },
+      fillColor: 0xffffff,
     });
 
-    this.button.anchor.set(0.5);
     this.button.x = app.screen.width / 2;
     this.button.y = app.screen.height / 2;
-
-    // 添加按钮点击事件处理程序
-    this.button.interactive = true;
-    this.button.buttonMode = true;
 
     this.background.addChild(this.button);
     this.addChild(this.background);
   }
 
   onClick(cb) {
-    this.button.on('pointerdown', (e) => {
+    this.button.onClick(() => {
       cb();
     });
   }
