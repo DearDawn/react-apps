5d711b4ef115462859e4b66baabc35235b9cf238
tangqiuqiu <dawn.tang@temu.com>
2024-04-25 18:24:25 +0800

    feat: 分享信息修改

--- src/utils/web.ts
+++ src/utils/web.ts
@@ -29,15 +29,37 @@ export function getFaviconUrl() {
   return 'https://www.dododawn.com/siteinfo/favicon.ico';
 }
 
+export function replaceFaviconUrl(newFaviconUrl) {
+  const faviconElement =
+    document.querySelector('link[rel="shortcut icon"]') ||
+    document.querySelector('link[rel="icon shortcut"]') ||
+    document.querySelector('link[rel="icon"]') ||
+    document.querySelector('link[href*="favicon.ico"]');
+
+  if (faviconElement) {
+    const faviconHref = faviconElement.getAttribute('href');
+    if (faviconHref) {
+      faviconElement.setAttribute('href', newFaviconUrl);
+    }
+  } else {
+    const newFaviconElement = document.createElement('link');
+    newFaviconElement.setAttribute('rel', 'shortcut icon');
+    newFaviconElement.setAttribute('href', newFaviconUrl);
+    document.head.appendChild(newFaviconElement);
+  }
+}
+
 export const changeShareInfo = ({
   title = document.title,
   description = document.title,
   url = location.href,
   image = getFaviconUrl(),
 }) => {
+  document.title = title;
   changeMetaTag('og:title', title);
   changeMetaTag('og:description', description);
   changeMetaTag('og:image', image);
   changeMetaTag('og:url', url);
   changeMetaTag('description', description, 'name');
+  replaceFaviconUrl(image);
 };
