88e6518a5fb5a64fb70f123fa807b5d7c3f5f0c2
tangqiuqiu <dawn.tang@temu.com>
2024-04-28 10:34:53 +0800

    feat: bugFix

--- src/utils/dev.ts
+++ src/utils/dev.ts
@@ -1,1 +1,5 @@
+export const query = new URL(window.location.href).searchParams;
+
 export const isDev = process.env.NODE_ENV === 'development';
+
+export const isTest = query.get('test') === '1';
--- src/utils/wx.ts
+++ src/utils/wx.ts
@@ -1,6 +1,6 @@
 import wx from 'weixin-js-sdk-ts';
 import { myPost } from './fetch';
-import { isDev } from './dev';
+import { isTest } from './dev';
 
 export const initWxSDK = () => {
   myPost(
@@ -11,7 +11,7 @@ export const initWxSDK = () => {
     .then((res: any) => {
       const { app_id, timestamp, noncestr, signature } = res || {};
       wx.config({
-        debug: isDev, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+        debug: isTest, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
         appId: app_id, // 必填，公众号的唯一标识
         timestamp, // 必填，生成签名的时间戳
         nonceStr: noncestr, // 必填，生成签名的随机串
