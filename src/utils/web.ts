cfed2490263a40c6f047ed55c5fb8613946abeb3
tangqiuqiu <dawn.tang@temu.com>
2024-04-28 11:44:09 +0800

    feat: bugFix

--- src/utils/web.ts
+++ src/utils/web.ts
@@ -82,6 +82,9 @@ export const changeShareInfo = ({
           desc: description, // 分享描述
           link: url, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
           imgUrl: image, // 分享图标
+          fail: function (err) {
+            console.log('[dodo] ', '分享设置失败', err);
+          },
           success: function () {
             console.log('[dodo] ', '分享设置成功');
             // 设置成功
