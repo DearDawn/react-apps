884542c89a3f7e4566ab96dd5747710dc3be7677
tangqiuqiu <dawn.tang@temu.com>
2024-06-17 21:40:35 +0800

    feat: moon

--- src/pages/demo/components/moon/index.tsx
+++ src/pages/demo/components/moon/index.tsx
@@ -5,7 +5,7 @@ import * as styles from './index.module.less';
 export const Moon: Comp = () => {
   const lunarAge = getLunarAge();
   const age = LUNAR_CYCLE_DAYS / 2 - lunarAge;
-  const ratio = age / LUNAR_CYCLE_DAYS;
+  const ratio = 1 - age / LUNAR_CYCLE_DAYS;
 
   return <div className={styles.moon} style={{ '--ratio': ratio } as any} />;
 };
